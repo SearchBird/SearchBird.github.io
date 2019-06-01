979f83f531d4dec65cc79dec072371e06aa3e29c
SearchBird <764064271@qq.com>
2019-06-01 22:41:15 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	new file:   css/index-ie.css
    	new file:   css/normalize.css
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   css/index-ie.css
    	modified:   css/index.css
    	modified:   css/main.css
    	modified:   index.html
    	modified:   myJs/index.js

--- myJs/index.js
+++ myJs/index.js
@@ -1,10 +1,26 @@
 $(function() {
+
+    // 解决edge兼容
+    decideEdge();
+    // 初始化动作
     onload();
 
 });
 
+// 解决edge兼容
+function decideEdge() {
+    var userAgent = navigator.userAgent;
+    var isEdge = userAgent.indexOf("Edge") > -1;
+    if (isEdge) {
+        $("#right-font").css("margin-right","-332px");
+    }
+}
+
+// 初始化动作
 function onload(){
+    // 初始化图片
     imgOnload();
+    // 初始化反色
     differenceOnload();
 }
 
