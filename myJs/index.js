70210cf001011439290556cc1fc7ad7f1c291ae3
SearchBird <764064271@qq.com>
2019-06-05 14:42:23 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   css/index.css
    	renamed:    css/index-ie.css -> css/piiic-main-ie.css
    	copied:     css/index.css -> css/piiic-main.css
    	copied:     index.html -> html/piiic-main.html
    	modified:   index.html
    	modified:   myJs/index.js
    	copied:     myJs/index.js -> myJs/piiic.js
    	new file:   scss/index.scss
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   css/index.css
    	modified:   html/piiic-main.html
    	modified:   index.html
    	modified:   myJs/piiic.js

--- myJs/index.js
+++ myJs/index.js
@@ -1,154 +1,136 @@
-$(function() {
+/**
+ * demo.js
+ */
 
-    // 解决浏览器兼容
-    compatibleLoad();
-    // 加载其他html文件
-    htmlLoad();
-    // 初始化样式
-    cssLoad();
-    // 滑动加载初始化
-    scrollLoad();
+var canvas = document.querySelector("#canvas"),
+    ctx = canvas.getContext("2d"),
+    link = document.createElement('link');
+    particles = [],
+    amount = 0,
+    mouse = { x: -9999, y: -9999 },
+    radius = 1,
+    colors = [
+      "rgba(0,222,255,0.85)", 
+      "rgba(12,253,74,0.75)", 
+      "rgba(193,253,12,0.85)", 
+      "rgba(192,213,255,0.85)", 
+      "rgba(244,223,254,0.75)"
+    ],
+    headline = document.querySelector("#headline"),
+    ww = window.innerWidth,
+    wh = window.innerHeight;
 
+function Particle(x, y) {
 
-});
-
-// 滑动加载初始化
-function scrollLoad() {
-    // 计算盒子高度，并且缓存
-    calculaHeight();
-    // 初始化加载
-    scrollInit();
-    // 初始化容器高度
-    containerInit();
+  this.x = Math.random() * ww;
+  this.y = Math.random() * wh;
+  this.dest = { x: x, y: y };
+  this.r = Math.random() * 2 * Math.PI;
+  this.vx = (Math.random() - 0.5) * 25;
+  this.vy = (Math.random() - 0.5) * 25;
+  this.accX = 0;
+  this.accY = 0;
+  this.friction = Math.random() * 0.025 + 0.94;
+  this.color = colors[Math.floor(Math.random() * 2.75)];
 }
 
-// 解决浏览器兼容
-function compatibleLoad() {
-    var userAgent = navigator.userAgent;
-    var isEdge = userAgent.indexOf("Edge") > -1;
-    var isQQ = userAgent.indexOf('QQBrowser') > -1;
-    var isFF = userAgent.indexOf("Firefox") > -1;
-    if (isEdge) {
-        $("#right-font").css("margin-right","-324px");
-        changeFont();
+Particle.prototype.render = function() {
+
+  this.accX = (this.dest.x - this.x) / 1000;
+  this.accY = (this.dest.y - this.y) / 1000;
+  this.vx += this.accX;
+  this.vy += this.accY;
+  this.vx *= this.friction;
+  this.vy *= this.friction;
+  this.x += this.vx;
+  this.y += this.vy;
+
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+  ctx.fill();
+
+  var a = this.x - mouse.x;
+  var b = this.y - mouse.y;
+
+  var distance = Math.sqrt(a * a + b * b);
+  if (distance < (radius * 75)) {
+
+    this.accX = (this.x - mouse.x) / 100;
+    this.accY = (this.y - mouse.y) / 100;
+    this.vx += this.accX;
+    this.vy += this.accY;
+  }
+}
+
+function onMouseMove(e) {
+
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+  }
+
+  function onTouchMove(e) {
+
+  if (e.touches.length > 0) {
+
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  }
+}
+
+function onTouchEnd(e) {
+
+  mouse.x = -9999;
+  mouse.y = -9999;
+}
+
+function initScene() {
+
+  ww = canvas.width = window.innerWidth;
+  wh = canvas.height = window.innerHeight;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'https://fonts.googleapis.com/css?family=Abril+Fatface';
+  document.getElementsByTagName('head')[0].appendChild(link);
+
+  ctx.font = 'bold 16vw "Abril Fatface"';
+  ctx.textAlign = "center";
+  ctx.fillText(headline.innerHTML, ww / 2, wh / 1.6);
+
+  var data = ctx.getImageData(0, 0, ww, wh).data;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.globalCompositeOperation = "screen";
+
+  particles = [];
+  for (var i = 0; i < ww; i += Math.round(ww / 200)) {
+    for (var j = 0; j < wh; j += Math.round(ww / 200)) {
+      if (data[((i + j * ww) * 4) + 3] > 200) {
+      
+        particles.push(new Particle(i, j));
+      }
     }
-    if (isQQ) {
-        changeFont();
-        $("#right-font").css("margin-right", "-343px");
-    }
-    if(isFF) {
-        $("#right-font").css("margin-right", "-347px;")
-    }
+  }
+  amount = particles.length;
 }
 
-// 初始化动作
-function cssLoad(){
-    // 初始化图片
-    imgOnload();
-    // 初始化反色
-    differenceOnload();
-    // 初始化盒子间距
-    distanceInit();
-    // 初始化攻击范围文字大小
-    attackScaleSizeInit();
+function render(a) {
+
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < amount; i++) {
+
+    particles[i].render();
+  }
 }
 
-// 加载其他html文件
-function htmlLoad() {
-    // 同步加载
-    syncHtml();
-    // 初始化高亮
-    highLightInit();
-}
-
-function attackScaleSizeInit() {
-    $attackScale = $(".attack-scale");
-    var sizeFlag = $.trim($attackScale.html()).length < 4;
-    if(sizeFlag) {
-        $attackScale.css("font-size", "17px");
-    }
-}
-
-function highLightInit() {
-    $(".heightLight").css("text-shadow", "2px 0rem 2px #BB3441,0rem 0rem .5rem #BB3441,0rem 0rem 0rem #BB3441,0rem 0rem 3px #BB3441");
-}
-
-function changeFont() {
-    var baseInfo = $(".base-info");
-    baseInfo.css("font-family","黑体");
-    baseInfo.css("font-weight","400");
-    $(".character-sign").css("font-family","黑体");
-}
-
-function distanceInit() {
-    var baseInfoHeight = parseInt($(".base-info").css("height").replace("px","")) + 155;
-    $(".maker").css("top",baseInfoHeight + "px");
-}
-
-function imgOnload() {
-    $img = $("img");
-
-    $img.lazyload({
-        effect : "fadeIn"
-    });
-}
-
-function differenceOnload(){
-    $(".difference").each(function(index, value) {
-        var $difference = $(value);
-
-        // 反色函数
-        $difference.css("background-color", new reversalColor($difference.css("background-color")).parse());
-        // 高亮处理
-        $difference.css("background-color", heightLight($difference));
-    });
-
-}
-
-// 亮度函数
-function heightLight($heightLight) {
-    var backgroundArr = $heightLight.css("background-color").replace(")","").replace("rgb(", "").split(", ");
-    for(var i = backgroundArr.length;i -- > 0;) {
-        var temp = backgroundArr[i];
-        var temp = parseInt(temp * 1.2);
-        backgroundArr[i] = temp > 255 ? 255 : temp;
-    }
-    return "rgb(" + backgroundArr.join(",") + ")";
-}
-
-// 通过遍历做缓存
-function calculaHeight(){
-    var divInitHeight = 1000;
-    globalDivHeight.push(divInitHeight);
-    for (var i = -1;i ++ < globalDiv.length - 1;){
-        var $globalDiv = $(globalDiv[i]);
-        divInitHeight += $globalDiv.outerHeight(true);
-        globalDivHeight.push(divInitHeight);
-    }
-}
-// 利用缓存中数据做绑定
-function scrollInit(){
-    $(window).scroll(function () {
-            htmlLazyLoad();
-    });
-}
-
-function syncHtml(synObj) {
-    for(var i = globalDiv.length;i -- >0;) {
-        var syncName = globalDiv[i];
-        $.ajax({
-            url: 'html/' + syncName.replace("#", "") + '.html',
-            type: 'get',
-            async: false,
-            success: function (res) {
-                $(syncName).html($(res));
-            }
-        });
-    }
-    //htmlLazyLoad();
-}
-
-function containerInit() {
-    $("#background-body").css("height", (globalDivHeight[globalDivHeight.length - 2] + parseInt($(".gs-content-word").height()) - 630) + "px");
-}
-
+headline.addEventListener("keyup", initScene);
+window.addEventListener("resize", initScene);
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("touchmove", onTouchMove);
+window.addEventListener("touchend", onTouchEnd);
+initScene();
+requestAnimationFrame(render);
---
+++ myJs/piiic.js
@@ -0,0 +1,154 @@
+$(function() {
+
+    // 解决浏览器兼容
+    compatibleLoad();
+    // 加载其他html文件
+    htmlLoad();
+    // 初始化样式
+    cssLoad();
+    // 滑动加载初始化
+    scrollLoad();
+
+
+});
+
+// 滑动加载初始化
+function scrollLoad() {
+    // 计算盒子高度，并且缓存
+    calculaHeight();
+    // 初始化加载
+    scrollInit();
+    // 初始化容器高度
+    containerInit();
+}
+
+// 解决浏览器兼容
+function compatibleLoad() {
+    var userAgent = navigator.userAgent;
+    var isEdge = userAgent.indexOf("Edge") > -1;
+    var isQQ = userAgent.indexOf('QQBrowser') > -1;
+    var isFF = userAgent.indexOf("Firefox") > -1;
+    if (isEdge) {
+        $("#right-font").css("margin-right","-324px");
+        changeFont();
+    }
+    if (isQQ) {
+        changeFont();
+        $("#right-font").css("margin-right", "-343px");
+    }
+    if(isFF) {
+        $("#right-font").css("margin-right", "-347px;")
+    }
+}
+
+// 初始化动作
+function cssLoad(){
+    // 初始化图片
+    imgOnload();
+    // 初始化反色
+    differenceOnload();
+    // 初始化盒子间距
+    distanceInit();
+    // 初始化攻击范围文字大小
+    attackScaleSizeInit();
+}
+
+// 加载其他html文件
+function htmlLoad() {
+    // 同步加载
+    syncHtml();
+    // 初始化高亮
+    highLightInit();
+}
+
+function attackScaleSizeInit() {
+    $attackScale = $(".attack-scale");
+    var sizeFlag = $.trim($attackScale.html()).length < 4;
+    if(sizeFlag) {
+        $attackScale.css("font-size", "17px");
+    }
+}
+
+function highLightInit() {
+    $(".heightLight").css("text-shadow", "2px 0rem 2px #BB3441,0rem 0rem .5rem #BB3441,0rem 0rem 0rem #BB3441,0rem 0rem 3px #BB3441");
+}
+
+function changeFont() {
+    var baseInfo = $(".base-info");
+    baseInfo.css("font-family","黑体");
+    baseInfo.css("font-weight","400");
+    $(".character-sign").css("font-family","黑体");
+}
+
+function distanceInit() {
+    var baseInfoHeight = parseInt($(".base-info").css("height").replace("px","")) + 155;
+    $(".maker").css("top",baseInfoHeight + "px");
+}
+
+function imgOnload() {
+    $img = $("img");
+
+    $img.lazyload({
+        effect : "fadeIn"
+    });
+}
+
+function differenceOnload(){
+    $(".difference").each(function(index, value) {
+        var $difference = $(value);
+
+        // 反色函数
+        $difference.css("background-color", new reversalColor($difference.css("background-color")).parse());
+        // 高亮处理
+        $difference.css("background-color", heightLight($difference));
+    });
+
+}
+
+// 亮度函数
+function heightLight($heightLight) {
+    var backgroundArr = $heightLight.css("background-color").replace(")","").replace("rgb(", "").split(", ");
+    for(var i = backgroundArr.length;i -- > 0;) {
+        var temp = backgroundArr[i];
+        var temp = parseInt(temp * 1.2);
+        backgroundArr[i] = temp > 255 ? 255 : temp;
+    }
+    return "rgb(" + backgroundArr.join(",") + ")";
+}
+
+// 通过遍历做缓存
+function calculaHeight(){
+    var divInitHeight = 1000;
+    globalDivHeight.push(divInitHeight);
+    for (var i = -1;i ++ < globalDiv.length - 1;){
+        var $globalDiv = $(globalDiv[i]);
+        divInitHeight += $globalDiv.outerHeight(true);
+        globalDivHeight.push(divInitHeight);
+    }
+}
+// 利用缓存中数据做绑定
+function scrollInit(){
+    $(window).scroll(function () {
+            htmlLazyLoad();
+    });
+}
+
+function syncHtml(synObj) {
+    for(var i = globalDiv.length;i -- >0;) {
+        var syncName = globalDiv[i];
+        $.ajax({
+            url: syncName.replace("#", "") + '.html',
+            type: 'get',
+            async: false,
+            success: function (res) {
+                $(syncName).html($(res));
+            }
+        });
+    }
+    //htmlLazyLoad();
+}
+
+function containerInit() {
+    $("#background-body").css("height", (globalDivHeight[globalDivHeight.length - 2] + parseInt($(".gs-content-word").height()) - 630) + "px");
+}
+
