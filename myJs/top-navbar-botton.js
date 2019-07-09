e49ca59e9fb31f496b77d00269a1c38416e9f5d9
SearchBird <764064271@qq.com>
2019-07-10 01:14:51 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   myJs/top-navbar-botton.js
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- myJs/top-navbar-botton.js
+++ myJs/top-navbar-botton.js
@@ -9,24 +9,23 @@ $("#character-name").focus(function() {
     }
 });
 
+// 回车和点击进行查询
 $("#character-name").keydown(function(e){
     if(e.keyCode==13) {
-        var value = $.trim($("#character-name").val());
-        if (value == "请输入干员名称,搜索长图" || !value) {
-            return;
-        }
-        else {
-            new reloadObj().reloadPiiic(value);
-        }
+        beginSearch();
     }
 })
-
 $(".search-piiic").click(function(){
+    beginSearch();
+})
+// 回车和点击进行查询
+
+function beginSearch() {
     var value = $.trim($("#character-name").val());
-    if(value == "请输入干员名称,搜索长图" || !value) {
+    if (value == "请输入干员名称,搜索长图" || !value) {
         return;
     }
     else {
         window.location.href = "piiic-main.html?code=" + value;
     }
-})
+}
