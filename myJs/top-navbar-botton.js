cc2a27cbed361e2374dea7fd3247e8878c27ac1a
SearchBird <764064271@qq.com>
2019-07-15 12:53:03 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   myJs/top-navbar-botton.js
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- myJs/top-navbar-botton.js
+++ myJs/top-navbar-botton.js
@@ -37,7 +37,7 @@ $("#shareStation").click(function(){
 
 $("#test").click(function() {
     $.ajax({
-        url: "https://49.234.4.31/uploadGithub",
+        url: "https://49.234.4.31:8081/uploadGithub",
         type: 'get',
         async: false,
         success: function (res) {
