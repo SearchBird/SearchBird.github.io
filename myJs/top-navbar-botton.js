7ab155efccaa88425cdea1385564e34f02a13399
SearchBird <764064271@qq.com>
2019-07-18 17:09:04 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   myJs/top-navbar-botton.js
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- myJs/top-navbar-botton.js
+++ myJs/top-navbar-botton.js
@@ -91,9 +91,9 @@ $("#test").click(function() {
 $("#character-name").focus(function() {
     var value = $.trim($("#character-name").val());
     if (!value) {
+        $("#character-name").val("请输入干员名称,搜索长图");
         $("#character-name").css("width", "210");
         $("#top-navbar-botton ul li.searchli").css("width", "252");
-        $("#top-navbar-botton ul li.searchli").attr("value", "请输入干员名称,搜索长图");
     }
 });
 $("#character-name").blur(function(){
