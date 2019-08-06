01e4bb6301e5e71a828b13392b7c66000b577733
SearchBird <764064271@qq.com>
2019-08-06 22:13:57 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	new file:   html/how2use.html
    	new file:   html/meltdown.html
    	new file:   myJs/how2use.js
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   css/index.css
    	modified:   html/how2use.html
    	modified:   html/introduction.html
    	modified:   index.html
    	modified:   myJs/how2use.js

---
+++ myJs/how2use.js
@@ -0,0 +1,39 @@
+var how2useClickFlag = false;
+
+function how2use() {
+
+    function init() {
+        initClick();
+    }
+
+    function initClick() {
+        $(".img-container-outter").click(function () {
+            how2useClickFlag = !how2useClickFlag;
+            if(how2useClickFlag) {
+                $(".index-use-title").css({
+                    "height" : "20%",
+                    "font-size" : "3rem",
+                })
+                $(".index-use-img-list").css({
+                    "height" : "80%"
+                })
+                $(".img-container").css({"overflow" : "visible", "width" : "80%"})
+                $(".img-content").css("display","none");
+                $("#img-prev2").css({"animation" : "prevAniLeft 1s 1 alternate forwards",});
+                $(".img-next:not(#img-next2)").css({"animation" : "prevAniLeft2 1s 1 alternate forwards",});
+                $("#img-next2").css({"animation" : "prevAniRight 1s 1 alternate forwards",});
+                $(".img-prev:not(#img-prev2)").css({"animation" : "prevAniRight2 1s 1 alternate forwards",});
+                document.styleSheets[0].deleteRule(0);
+                document.styleSheets[0].deleteRule(0);
+            }
+        })
+    }
+
+    return {
+        init : init,
+    }
+}
+
+setTimeout(function () {
+    how2use().init();
+}, 500);
